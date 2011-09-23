796f93b29feaf91799bba771c220dafeaa6c38d0
Roger Jungemann <roger@thefifthcircuit.com>
2011-09-22 21:40:44 -0700

    added version to formula

--- formulas/tfc_git_commands.rb
+++ formulas/tfc_git_commands.rb
@@ -3,6 +3,7 @@ require 'formula'
 class TfcGitCommands < Formula
   url 'https://github.com/thefifthcircuit/tfc_git_commands.git'
   homepage 'https://github.com/thefifthcircuit/tfc_git_commands'
+  version 'v0.0.1'
 
   depends_on 'octokit' => :ruby
 
